6556e0a2898d3657bb74ff47e64f8d8538b39b55
Yuming Cui <yuming.cui@gmail.com>
2019-10-05 10:43:02 -0400

    Fix a bug for empty  filtered dataset.

--- UI.R
+++ UI.R
@@ -1,18 +1,6 @@
 library(leaflet)
 library(shinyWidgets)
 
-vars <- c(
-  "1 room" = 1,
-  "2 rooms" = 2,
-  "3 rooms" = 3,
-  "4 rooms" = 4,
-  "5 rooms" = 5,
-  "6 rooms" = 6,
-  "7 rooms" = 7,
-  "8 rooms" = 8,
-  "9 rooms" = 9,
-  "10+ rooms" = 10
-)
 navbarPage("Melbourne Housing", id="nav",
           
            tabPanel("Interactive map",
@@ -33,21 +21,47 @@ navbarPage("Melbourne Housing", id="nav",
                                       width = 350, height = "auto",
                                       
                                         h2("Filters"),
-                                        noUiSliderInput(inputId = "price", label = "Price Range", format = wNumbFormat(decimals = 0,
-                                                                                                              thousand = ",",
-                                                                                                              prefix = ""), min = 185000, max = 2070000, step = 10000, value = c(185000, 2070000)),
+                                        noUiSliderInput(inputId = "price", 
+                                                        label = "Price Range", 
+                                                        format = wNumbFormat(decimals = 0,
+                                                                             thousand = ",",
+                                                                             prefix = "$"), 
+                                                        min = 185000, 
+                                                        max = 2070000, 
+                                                        step = 10000, 
+                                                        value = c(185000, 2070000)),
                                   
-                                        noUiSliderInput(inputId = "rooms", label = "Number of Rooms", format =wNumbFormat(decimals = 0,
-                                                                                                                  thousand = ",",
-                                                                                                                  prefix = ""), min = 1, max = 12, step = 1, value = c(2, 4)),
+                                        noUiSliderInput(inputId = "rooms", 
+                                                        label = "Number of Rooms", 
+                                                        format =wNumbFormat(decimals = 0,
+                                                                            thousand = ",",
+                                                                            prefix = ""), 
+                                                        min = 1, 
+                                                        max = 12, 
+                                                        step = 1, 
+                                                        value = c(2, 4)),
                                         
-                                        noUiSliderInput(inputId = "year", label = "Build Year",  format = wNumbFormat(decimals = 0,
-                                                                                                             thousand = "",
-                                                                                                             prefix = ""), min = 1830, max = 2018, step = 1, value = c(1990, 2000)),
+                                        noUiSliderInput(inputId = "year", 
+                                                        label = "Build Year",  
+                                                        format = wNumbFormat(decimals = 0,
+                                                                             thousand = "",
+                                                                             prefix = ""), 
+                                                        min = 1830, 
+                                                        max = 2018, 
+                                                        step = 1, 
+                                                        value = c(1990, 2000)),
                                     
-                                        noUiSliderInput(inputId = "distance", label = "Distance to CBD", format = wNumbFormat(decimals = 1,
-                                                                                                                     thousand = ",",
-                                                                                                                     prefix = ""), min = 0, max = 50, step = 0.5, value = c(0, 10))
+                                        noUiSliderInput(inputId = "distance", 
+                                                        label = "Distance to CBD  ", 
+                                                        format = wNumbFormat(decimals = 1,
+                                                                             thousand = ",",
+                                                                             suffix = " Km"), 
+                                                        min = 0, 
+                                                        max = 50, 
+                                                        step = 0.1, 
+                                                        value = c(0, 10)),
+                                      
+                                      plotOutput("scatterDistancePrice", height = 250)
                         ),
                         
                         
