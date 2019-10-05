627cdb47f59b43d137c09380829827a4149eb6ad
Yuming Cui <yuming.cui@gmail.com>
2019-10-04 23:56:24 -0400

    Using a new slider.

--- UI.R
+++ UI.R
@@ -1,4 +1,5 @@
 library(leaflet)
+library(shinyWidgets)
 
 vars <- c(
   "1 room" = 1,
@@ -28,19 +29,25 @@ navbarPage("Melbourne Housing", id="nav",
                         
                         # Shiny versions prior to 0.11 should use class = "modal" instead.
                         absolutePanel(id = "controls", class = "panel panel-default", 
-                                      draggable = FALSE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                      width = 330, height = "auto",
+                                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                      width = 350, height = "auto",
                                       
-                                      h2("Filters"),
-                                      sliderInput("price", "Price Range", 185000, 2070000, value = c(185000, 2070000), step = 10000, sep = "", dragRange = FALSE),
+                                        h2("Filters"),
+                                        noUiSliderInput(inputId = "price", label = "Price Range", format = wNumbFormat(decimals = 0,
+                                                                                                              thousand = ",",
+                                                                                                              prefix = ""), min = 185000, max = 2070000, step = 10000, value = c(185000, 2070000)),
                                   
-                                        sliderInput("rooms", "Number of Rooms", 1, 12, value = c(2, 4), sep = "",  dragRange = FALSE),
+                                        noUiSliderInput(inputId = "rooms", label = "Number of Rooms", format =wNumbFormat(decimals = 0,
+                                                                                                                  thousand = ",",
+                                                                                                                  prefix = ""), min = 1, max = 12, step = 1, value = c(2, 4)),
                                         
-                                        sliderInput("year", "Build Year", 1830, 2018, value = c(1990, 2000), sep = "",  dragRange = FALSE),
+                                        noUiSliderInput(inputId = "year", label = "Build Year",  format = wNumbFormat(decimals = 0,
+                                                                                                             thousand = "",
+                                                                                                             prefix = ""), min = 1830, max = 2018, step = 1, value = c(1990, 2000)),
                                     
-                                        sliderInput("distance", "Distance to CBD", 0, 50, value = c(0, 10), sep = "",  dragRange = FALSE)
-                                      
-                
+                                        noUiSliderInput(inputId = "distance", label = "Distance to CBD", format = wNumbFormat(decimals = 1,
+                                                                                                                     thousand = ",",
+                                                                                                                     prefix = ""), min = 0, max = 50, step = 0.5, value = c(0, 10))
                         ),
                         
                         
